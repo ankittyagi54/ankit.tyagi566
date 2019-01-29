11b8e27700459b28d9cc036f03de6e376864a40b
ankittyagi54 <tyagiankit54@gmail.com>
2019-01-29 16:49:26 +0530

    second commit

---
+++ src/jenkinsproject_package/JenkinsTestng.java
@@ -0,0 +1,34 @@
+package jenkinsproject_package;
+
+import org.testng.annotations.Test;
+
+public class JenkinsTestng {
+	
+	
+	  
+	  @Test                                                                                                           // @BeforeMethod
+	    public void variable()
+	{
+		System.out.println("my first test case");
+	}
+	  
+	  
+	  
+	  @Test                                                                                                           // @BeforeMethod
+	    public void ankit()
+	{
+		System.out.println("my first test case");
+		
+		
+	}
+	  
+	  @Test                                                                                                           // @BeforeMethod
+	    public void diplay()
+	{
+		System.out.println("my first test case");
+		
+	}
+	  
+	  
+ 
+}
